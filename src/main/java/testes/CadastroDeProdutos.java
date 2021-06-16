98ead3fcfe3acd97569ba68628aeb31c7926b8cc
Felipe <felipeffggomes@hotmail.com>
2021-06-16 18:45:15 -0300

    Criação do Projeto e ConfiguracoesEntityManager

---
+++ src/main/java/testes/CadastroDeProdutos.java
@@ -0,0 +1,27 @@
+package testes;
+
+import modelo.Produto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+
+public class CadastroDeProdutos {
+
+    public static void main(String[] args) {
+
+        Produto celular = new Produto();
+        celular.setNome("Xiaomi redmi");
+        celular.setDescricao("Muito Legal");
+        celular.setPreco(new BigDecimal("800"));
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
+        EntityManager em = factory.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(celular);
+        em.getTransaction().commit();
+        em.close();
+    }
+}
